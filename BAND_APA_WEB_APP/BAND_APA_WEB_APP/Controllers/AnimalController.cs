5eca1cdbc980582a40a778b2914d6525d929f942
DuarteDosSantosNuno <67701344+DuarteDosSantosNuno@users.noreply.github.com>
2021-12-27 14:34:51 +0100

    V0.0.1 changement d'architecture du projet

---
+++ BAND_APA_WEB_APP/BAND_APA_WEB_APP/Controllers/AnimalController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetFinalWebApp.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: AnimalController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AnimalController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AnimalController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AnimalController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnimalController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AnimalController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnimalController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AnimalController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BAND_APA_WEB_APP/BAND_APA_WEB_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetFinalWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
